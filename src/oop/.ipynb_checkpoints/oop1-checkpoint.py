277dc28f0b4dd4dd978a484ef825d9a2cfcc0663
johnwesleyharding <johnwesleyharding@users.noreply.github.com>
2020-04-10 09:52:16 -0600

    oop and comp

--- src/oop/.ipynb_checkpoints/oop1-checkpoint.py
+++ src/oop/.ipynb_checkpoints/oop1-checkpoint.py
@@ -17,26 +17,3 @@
 #     pass
 #
 # Put a comment noting which class is the base class
-
-# The base class is Vehicle
-
-class Vehicle:
-    pass
-
-class GroundVehicle(Vehicle):
-    pass
-
-class FlightVehicle(Vehicle):
-    pass
-
-class Car(GroundVehicle):
-    pass
-
-class Motorcycle(GroundVehicle):
-    pass
-
-class Airplane(FlightVehicle):
-    pass
-
-class Starship(FlightVehicle):
-    pass
